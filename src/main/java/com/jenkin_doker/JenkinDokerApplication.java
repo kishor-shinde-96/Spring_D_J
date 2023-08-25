64e78a285a8c0dfc3a598d0a5b243c3e0d3d420b
Kishor <kishor.shinde@austere.co.in>
2023-08-25 17:34:44 +0530

    first commit

---
+++ src/main/java/com/jenkin_doker/JenkinDokerApplication.java
@@ -0,0 +1,13 @@
+package com.jenkin_doker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkin_doker/JenkinDokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkin_doker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinDokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
